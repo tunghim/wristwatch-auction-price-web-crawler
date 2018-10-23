77c5b18078c912e6bb2233b7b372ecc27b03ddee
Tunghim Lam <tunghim@gmail.com>
2018-10-24 03:22:23 +0800

    Rm special characters

--- handle_unicode.py
+++ handle_unicode.py
@@ -1,7 +1,12 @@
+import re
 import json
 
 with open('raw.json') as f:
     data = json.load(f)
+
+for i in data:
+    for k, v in i.items():
+        i[k] = re.sub('(\\r|\\n|\\r\\n)', '', v)
 
 with open('result.json', 'w+') as f:
     json.dump(data, f, ensure_ascii=False)
